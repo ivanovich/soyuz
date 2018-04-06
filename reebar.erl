2114adcc267f3be7ba40ac82b43b11bd62a644c4
L. Ivanovich Harrison <ivanovich@cock.li>
2018-04-05 18:37:58 -0700

    misc tweaks

--- reebar.erl
+++ reebar.erl
@@ -1,8 +1,9 @@
-%% This is a companion module to rebar.config.script, going through
-%% compile:file/1 to take record definitions from a header and
-%% put them into erlydtl_opts, so templates may refer to records.
-%% There's probably a more principled way to do this, but it works
-%% and that's all I care about.
+%% Since rebar.config.script uses file:script/2 which cannot accept
+%% preprocessor directives, our rebar.config.script takes this module
+%% and puts it through compile:file/1, to take record definitions from 
+%% a header and put them into erlydtl_opts, so templates are equipped 
+%% with record information. There's probably a more principled way to
+%% do this, but it works and that's all I care about.
 
 -module(reebar).
 -export([conf/0]).
